bfbb320e8cafb8354c8a4332c26586cf6cdafd9e
niteesh hegde <niteeshhegde343@gmail.com>
2022-08-19 10:14:19 +0530

    commit

---
+++ src/FurnitureMart.java
@@ -0,0 +1,28 @@
+public class FurnitureMart {
+    public static void main(String[] args) {
+        String table;
+        String chair;
+        String cupboards;
+        String stool;
+       color();
+       grade();
+       IndoorAndOutdoor();
+
+
+    }
+    public static void color(){
+
+
+    }
+    public static void grade(){
+
+    }
+    public static void IndoorAndOutdoor(){
+
+    }
+
+
+
+
+
+}
